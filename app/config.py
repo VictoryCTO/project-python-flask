97f629fa4b3c0e9836d83b0a15a96ff6687222ca
ScotterMonk <scotter@protonmail.com>
2024-10-15 19:31:29 -0500

    Issue-05-Fix-our-secrets-fin

--- app/config.py
+++ app/config.py
@@ -1,9 +1,15 @@
 import os
+from dotenv import load_dotenv
 
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "mysecret")
+    # SECRET_KEY = os.getenv("SECRET_KEY", "mysecret")
+    # old: SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///users.db")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    # old:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///users.db")
+    # new:
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
@@ -17,6 +23,7 @@ class ProductionConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"  # In-memory DB for testing
+    # SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI', 'sqlite:///:memory:')
+    SQLALCHEMY_DATABASE_URI = os.getenv("TEST_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = False  # Disable CSRF for easier testing
